3692ddfafffbb3861e723868520a1e46c61c1866
Leonardo dos Santos Duarte Silva <ldsds94@gmail.com>
2021-09-24 11:26:37 -0300

    feat: updated PostList component

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,12 +1,28 @@
 import { PostListItem } from './Post';
 import styles from './styles.module.scss';
 
-export function PostList(): JSX.Element {
+// Deixo aqui essa interface ou crio um arquivo de tipos pra ela?
+// Os tipos são iguais e o intuito de uso também é o mesmo
+interface Post {
+  uid?: string;
+  first_publication_date: string | null;
+  data: {
+    title: string;
+    subtitle: string;
+    author: string;
+  };
+}
+
+interface PostListProps {
+  posts: Post[];
+}
+
+export function PostList({ posts }: PostListProps): JSX.Element {
   return (
     <ul className={styles.listContainer}>
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
+      {posts.map(post => (
+        <PostListItem post={post} />
+      ))}
     </ul>
   );
 }
